94fde5ab47f27c00ea0235c8bd6dc292776cbb66
EstomJr <estonjr2@hotmail.com>
2026-02-02 01:25:31 -0300

    fix: update UserRepository signature and imports

--- auth-service/src/main/java/com/urbpe/auth_service/repository/UserRepository.java
+++ auth-service/src/main/java/com/urbpe/auth_service/repository/UserRepository.java
@@ -7,5 +7,5 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<UserDetails> findeUserByEmail(String username);
+   User findUserByEmail(String email);
 }
