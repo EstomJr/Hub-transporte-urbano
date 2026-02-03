ab25d7048617efc817e4780aa0a6770c37149c8d
EstomJr <estonjr2@hotmail.com>
2026-02-03 03:45:42 -0300

    fix: correcting the table name

--- auth-service/src/main/resources/db/migration/V2_create_insert_user.sql
+++ auth-service/src/main/resources/db/migration/V2_create_insert_user.sql
@@ -1,4 +1,4 @@
-CREATE TABLE tb_users (
+CREATE TABLE users (
                           id SERIAL PRIMARY KEY,
                           name VARCHAR(255) NOT NULL,
                           email VARCHAR(255) NOT NULL UNIQUE,
@@ -6,6 +6,6 @@ CREATE TABLE tb_users (
                           role VARCHAR(20) NOT NULL
 );
 
-INSERT INTO tb_users (name, email, password, role) VALUES
+INSERT INTO users (name, email, password, role) VALUES
                                                        ('Administrador', 'admin@email.com', '$2a$10$8.UnVuG9HHgffUDAlk8qfOpNa.hPAxF7p.tvPpmtbJDfyqWn3k7.6', 'ADMIN'),
                                                        ('Usuario Comum', 'user@email.com', '$2a$10$8.UnVuG9HHgffUDAlk8qfOpNa.hPAxF7p.tvPpmtbJDfyqWn3k7.6', 'USER');
